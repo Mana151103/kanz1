b741d45e5dcc6545e2c9d6a7465884452a0d9080
m.osada <af23072@shibaura-it.ac.jp>
2025-06-16 19:11:34 +0900

    first commit

---
+++ readData.h
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<cstdlib>
+#include<cmath>
+#include<vector>
+#include<climits>
+#include<string>
+#include<cfloat>
+#include<random>
+#include<algorithm>
+#include<sys/stat.h>
+#include<map>
+#include<filesystem>
+#include"vectorArray.h"
+
+#ifndef __READDATA__
+#define __READDATA__
+
+VectorArray readTestData(const std::string &file, int dim);
+Vector readTestCorrect(const std::string &file);
+
+void recordClassified(const VectorArray &data, const Vector &label);
+
+Vector read_data(const std::string &fileName, VectorArray &data);
+Vector read_dataWithoutLabel(const std::string &fileName, VectorArray &data);
+void read_data2(const std::string &fileName, VectorArray &data, Vector &label, int dimension);
+void read_data3(const std::string &fileName, VectorArray &data, Vector &label);
+#endif
